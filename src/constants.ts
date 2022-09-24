fc7e927255d3e1609dc03d52d429530056929476
Tudor <tudor.tuglea@gmail.com>
2022-09-24 18:04:57 +0300

    Added async interaction

---
+++ src/constants.ts
@@ -0,0 +1,10 @@
+import { GatewayIntentsString } from "discord.js";
+
+export const intents: GatewayIntentsString[] = [
+  "DirectMessages",
+  "GuildInvites",
+  "GuildMembers",
+  "Guilds",
+  "MessageContent",
+  "GuildMessages",
+];
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,35 @@
-import { Message } from "discord.js";
+import axios from "axios";
+import { Message, ModalBuilder } from "discord.js";
 
 require("dotenv").config();
 import { Client } from "discord.js";
+import { intents } from "./constants";
+
+const API_URL = "https://rickandmortyapi.com/api/character/?name=";
 
 const client = new Client({
-  intents: [
-    "DirectMessages",
-    "GuildInvites",
-    "GuildMembers",
-    "Guilds",
-    "MessageContent"
-  ],
+  intents
 });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user?.tag}`);
 });
 
-client.on("messageCreate", (msg: Message) => {
-  console.log(msg.content)
+client.on("messageCreate", async (msg: Message) => {
+  console.log(msg.content);
   if (msg.content == "pong") {
     msg.reply("ping");
+    return;
+  }
+  if (msg.content.includes("Tell me about")) {
+    const character = msg.content.split(" ")[3];
+    const resp = await axios(`${API_URL}${character}`);
+    const data = resp.data;
+    const charInfo = data.results[0];
+    msg.reply(
+      `His name is: ${charInfo.name}, he is ${charInfo.status}, his home planet is ${charInfo.origin.name}`
+    );
+    return;
   }
 });
 
