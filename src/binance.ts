66a7b297b70a8685a8b114efe78aedcaeadd7ce9
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-09-25 19:09:55 +0300

    implemented a chart for the last 5 minutes of a specific coin's price

--- src/binance.ts
+++ src/binance.ts
@@ -1,17 +1,63 @@
 import axios from "axios";
 import { API_BINANCE_URL } from "./constants";
 
-export type BinanceDataResponse = {
-  minutes: string;
-  price: number;
-}
+type KLine = {
+  openTime: number;
+  openPrice: string;
+  highPrice: string;
+  lowPrice: string;
+  volume: string;
+  closePrince: string;
+  closeTime: number;
+  assetVolume: string;
+  numberOfTrades: number;
+  takerBuyBaseAssetVolume: string;
+  takerBuyBaseQuoteVolume: string;
+  unusedFieldIgnore: string;
+};
 
-export const fetchBinanceData = async (coin: string, currency: string): Promise<BinanceDataResponse> => {
-  const resp = await axios(
-    `${API_BINANCE_URL}/avgPrice?symbol=${coin}${currency}`
+type KLineTuple = [
+  number,
+  string,
+  string,
+  string,
+  string,
+  string,
+  number,
+  string,
+  number,
+  string,
+  string,
+  string
+];
+
+export const fetchBinanceData = async (
+  coin: string,
+  currency: string
+): Promise<KLine[]> => {
+  const KLines: KLine[] = [];
+
+  const response = await axios(
+    `${API_BINANCE_URL}/klines?symbol=${coin}${currency}&interval=1m&limit=5`
   );
-    return {
-        minutes: resp.data.mins,
-        price: resp.data.price
+
+  response.data.map((item: KLineTuple) => {
+    const kLineData = {
+      openTime: item[0],
+      openPrice: item[1],
+      highPrice: item[2],
+      lowPrice: item[3],
+      closePrince: item[4],
+      volume: item[5],
+      closeTime: item[6],
+      assetVolume: item[7],
+      numberOfTrades: item[8],
+      takerBuyBaseAssetVolume: item[9],
+      takerBuyBaseQuoteVolume: item[10],
+      unusedFieldIgnore: item[11],
     };
+
+    KLines.push(kLineData);
+  });
+  return KLines;
 };
--- src/interactions/crypto.ts
+++ src/interactions/crypto.ts
@@ -1,16 +1,60 @@
-import { CacheType, ChatInputCommandInteraction, Interaction } from "discord.js";
+import { CacheType, ChatInputCommandInteraction } from "discord.js";
 import { fetchBinanceData } from "../binance";
+import { Canvas } from "canvas";
+
+import { Chart } from "chart.js";
 
 export const getCryptoData = async (
   interaction: ChatInputCommandInteraction<CacheType>
 ) => {
   const coin = interaction.options.getString("coin");
   const currency = interaction.options.getString("currency");
+
   if (!coin || !currency) return;
 
-  const { price } = await fetchBinanceData(coin, currency);
+  const prices = await fetchBinanceData(coin, currency);
 
-  await interaction.reply(
-    `Last recorded transaction was 1 ${coin} for ${price} ${currency}`
-  );
+  const canvas = new Canvas(700, 250);
+  const ctx = canvas.getContext("2d");
+
+  const closeTime = prices.map((item) => {
+    const date = new Date(item.closeTime);
+    return `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  });
+  const closePrice = prices.map((item) => Number(item.closePrince));
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: closeTime,
+      datasets: [
+        {
+          label: `${coin} prices`,
+          showLine: true,
+          data: closePrice,
+          backgroundColor: [
+            "rgba(255, 99, 132)",
+            "rgba(54, 162, 235)",
+            "rgba(255, 206, 86)",
+            "rgba(75, 192, 192)",
+            "rgba(153, 102, 255)",
+            "rgba(255, 159, 64)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+  });
+
+  await interaction.reply({
+    files: [{ attachment: canvas.toBuffer() }],
+  });
 };
